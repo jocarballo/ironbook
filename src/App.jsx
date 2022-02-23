8f40f0d90dd96df7897d22c0d67f9c6e75515e5e
Joana Carballo <joana16cruz@gmail.com>
2022-02-23 17:03:23 +0100

    iteration 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,41 @@
 import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
+import users from "./users";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [ironhackersList, setIronhackersList] = useState(users)
+  console.log(users)
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <div className="ironhackersTable">
+      <h1>Iron Book</h1>
+      <table>
+      <tbody>
+        <tr>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Campus</th>
+          <th>Role</th>
+          <th>Links</th>
+        </tr>
+        {ironhackersList.map((val, key) => {
+          return (
+            <tr key={key}>
+              <td>{val.firstName}</td>
+              <td>{val.lastName}</td>
+              <td>{val.campus}</td>
+              <td>{val.role}</td>
+              <td>{val.linkedin && "📝"}</td>
+            </tr>
+           
+          )
+        })}
+        </tbody>
+      </table>
+      </div>
     </div>
   )
 }
